b619438940236090c6e8a2c8a42b6158971ba547
Dan Hassin <danhassin@mac.com>
2016-10-26 11:23:29 -0400

    Add NavigationCard constants

---
+++ src/Libraries/NavigationExperimental/NavigationCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import createMockComponent from '../../components/createMockComponent';
+
+class CardStackPanResponder {
+}
+
+class PagerPanResponder {
+}
+
+const NavigationCard = createMockComponent('NavigationCard');
+
+NavigationCard.CardStackPanResponder = CardStackPanResponder;
+NavigationCard.CardStackStyleInterpolator = {
+  forHorizontal: () => ({}),
+  forVertical: () => ({}),
+};
+NavigationCard.PagerPanResponder = PagerPanResponder;
+NavigationCard.PagerStyleInterpolator = {
+  forHorizontal: () => ({}),
+};
+
+module.exports = NavigationCard;
