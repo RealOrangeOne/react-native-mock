e0623d226ebc06de2b2ee5b782128dc5935d356e
Jake Howard <git@theorangeone.net>
2016-12-14 16:48:44 +0000

    More tests on global

--- tests/globals.test.js
+++ tests/globals.test.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import defineGlobalProperty from '../src/defineGlobalProperty';
 
 
 describe('Globals', function () {
@@ -6,11 +7,18 @@ describe('Globals', function () {
     expect(global.Promise).to.deep.equal(require('promise'));
   });
 
-  it('should have promise globally', function () {
+  it('should have regenerator runtime globally', function () {
     expect(global.regeneratorRuntime).to.deep.equal(require('regenerator-runtime/runtime'));
   });
 
   it('should be in dev', function () {
     expect(global.__DEV__).to.be.true;
+  });
+
+  describe('describeGlobalProperty', function () {
+    it('Should define property', function () {
+      defineGlobalProperty('testKey1', 'testValue1');
+      expect(global.testKey1).to.equal('testValue1');
+    });
   });
 });
