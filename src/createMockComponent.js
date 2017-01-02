d0e99aecfe13603f793502262f65f0e9ead07dbc
Jake Howard <git@theorangeone.net>
2017-01-02 12:39:54 +0000

    FIx mock component display name

--- src/createMockComponent.js
+++ src/createMockComponent.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default name => {
   const RealComponent = require(name);
-  const componentName = (RealComponent.displayName || RealComponent.name || name).replace(/^(RCT|RK)/, '');
-  class Component extends RealComponent {  // eslint-disable-line react/prefer-stateless-function
+  const realComponentName = RealComponent.name === 'Component' ? name : RealComponent.name
+  const componentName = (RealComponent.displayName || realComponentName || name).replace(/^(RCT|RK)/, '');
+
+  return class extends RealComponent {  // eslint-disable-line react/prefer-stateless-function
+    static displayName = componentName;
+    static name = componentName;
+
     render() {
       return React.createElement(
         componentName,
@@ -11,6 +16,5 @@ export default name => {
         this.props.children
       );
     }
-  }
-  return Component;
+  };
 };
