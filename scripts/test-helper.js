6b6106ece7e29670f4182614eb902fb65fd49460
Jake Howard <git@theorangeone.net>
2017-01-02 12:50:33 +0000

    Add / setup jsdom

--- scripts/test-helper.js
+++ scripts/test-helper.js
@@ -1,5 +1,22 @@
 const sinonChai = require('sinon-chai');
 const chai = require('chai');
+const jsdom = require('jsdom');
 
 chai.expect();
 chai.use(sinonChai);
+
+// Jsdom document & window
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+// Add to global
+global.document = doc;
+global.window = win;
+
+
+// Add window keys to global window
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
