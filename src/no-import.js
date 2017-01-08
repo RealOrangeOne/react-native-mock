07974bf9d104dcdc74ebc03337357169486affba
Jake Howard <git@theorangeone.net>
2017-01-08 17:10:05 +0000

    Add script to stop requiring

---
+++ src/no-import.js
@@ -0,0 +1,7 @@
+console.error(`
+Looks like you're trying to require react-native-mock directly.
+You dont need to do this to access the mocked version of react-native.
+Please re-check the instructions in the README for how to use it correctly!
+`);
+
+process.exit(1);
