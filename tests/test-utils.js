80d305dd8c9e289a23aec2f5f162d15f3708290c
Jake Howard <git@theorangeone.net>
2017-03-03 09:46:42 +0000

    Improve spy checks

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -8,4 +8,8 @@ export const expectSpy = function isSpy(spy) {
   expect(spy).to.contain.all.keys(
     Object.keys(sinon.spy())
   );
+  expect(spy.id).to.contain('spy');
+  expect(spy.callCount).to.be.a('number');
+  expect(spy.called).to.be.a('boolean');
+  expect(spy.notCalled).to.be.a('boolean');
 };
