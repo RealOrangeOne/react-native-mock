0f6b9523a51dffd5897ab943e79ff0aefd00a837
Jake Howard <git@theorangeone.net>
2017-03-04 19:54:15 +0000

    Cover main import handler

--- src/no-import.js
+++ src/no-import.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-console */
 
-console.error(`
+// eslint-disable-next-line no-throw-literal
+throw `
 Looks like you're trying to require react-native-mock directly.
 You dont need to do this to access the mocked version of react-native.
 Please re-check the instructions in the README for how to use it correctly!
-`);
-
-process.exit(1);
+`;
