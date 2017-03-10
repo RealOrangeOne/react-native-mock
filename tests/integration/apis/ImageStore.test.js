43603123815ad0dcc510fac60c2b01407dbf5686
Jake Howard <git@theorangeone.net>
2017-03-10 08:52:21 +0000

    Add ImageStore tests

---
+++ tests/integration/apis/ImageStore.test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+describe('ImageStore', () => {
+  const { ImageStore } = require('react-native');
+
+  it('should have right functions', function () {
+    expect(ImageStore.hasImageForTag).to.be.a('function');
+    expect(ImageStore.removeImageForTag).to.be.a('function');
+    expect(ImageStore.addImageFromBase64).to.be.a('function');
+    expect(ImageStore.getBase64ForTag).to.be.a('function');
+  });
+});
