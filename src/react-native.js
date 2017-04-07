f98616a93bed0ce2a1cd6e7d7d79f548963ae8b4
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-04-07 17:20:23 +0200

    fix: remove deprecated addons package

--- src/react-native.js
+++ src/react-native.js
@@ -107,16 +107,16 @@ const ReactNative = {
 
 // See http://facebook.github.io/react/docs/addons.html
 const ReactNativeAddons = {
-  // LinkedStateMixin: require('react-addons-linked-state-mixin') deprecated,
+  // LinkedStateMixin: require('react-addons-linked-state-mixin'), deprecated
   Perf: require('react-addons-perf'),
-  PureRenderMixin: require('react-addons-pure-render-mixin'),
+  // PureRenderMixin: require('react-addons-pure-render-mixin'), deprecated
   TestModule: require('./NativeModules/TestModule'),
   TestUtils: require('react-addons-test-utils'),
   // TODO(lmr): not sure where to find this
   // batchedUpdates: require('ReactUpdates').batchedUpdates, deprecated
   // cloneWithProps: require('react-addons-clone-with-props'), deprecated
-  createFragment: require('react-addons-create-fragment'),
-  update: require('react-addons-update'),
+  // createFragment: require('react-addons-create-fragment'), deprecated
+  // update: require('react-addons-update'), deprecated
 };
 
 Object.assign(ReactNative, React, { addons: ReactNativeAddons });
