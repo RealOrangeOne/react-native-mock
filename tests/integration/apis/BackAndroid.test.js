513d44a7a40cee56809249a6e182d6caa028e5fb
Jason Ma <jason@peartherapeutics.com>
2017-11-05 11:57:02 -0800

    Fix BackAndroid and add BackHandler - BackAndroid is deprecated by BackHandler - added test for BackHandler - Remove obsolete test of `addEventListener().remove()`

--- tests/integration/apis/BackAndroid.test.js
+++ tests/integration/apis/BackAndroid.test.js
@@ -1,20 +1,11 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 
 describe('BackAndroid', () => {
   const { BackAndroid } = require('react-native');
 
   it('should bind events', function () {
     expect(BackAndroid.addEventListener).to.be.a('function');
-    expect(BackAndroid.addEventListener().remove).to.be.a('function');
     expect(BackAndroid.removeEventListener).to.be.a('function');
     expect(BackAndroid.exitApp).to.be.a('function');
-  });
-
-  it('should remove', function () {
-    sinon.stub(BackAndroid, 'removeEventListener');
-    BackAndroid.addEventListener().remove();
-    expect(BackAndroid.removeEventListener).to.have.been.calledOnce;
-    BackAndroid.removeEventListener.restore();
   });
 });
